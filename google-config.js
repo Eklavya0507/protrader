12128e9a15181b12295fd2d13216b444b9fa2e10
Eklavya0507 <gautamkamar96970@gmail.com>
2026-06-24 14:47:26 +0530

    Update google-config.js

--- google-config.js
+++ google-config.js
@@ -1,4 +1,4 @@
 // Google OAuth Web Client ID is public and safe to use in frontend code.
 // Paste the Client ID created in Google Cloud Console below.
 window.PROTRADE_GOOGLE_CLIENT_ID =
-  "PASTE_YOUR_GOOGLE_WEB_CLIENT_ID.apps.googleusercontent.com";
+  "549565823292-rqqbu007k2r5c62sp8ar078qavc35vcj.apps.googleusercontent.com";
